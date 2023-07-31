85cf4b875ef9e08f9b7262a0ae5dd0be7f6d12f9
cmauget <clement.mauget@gmal.com>
2023-07-31 11:42:13 +0200

    💩

--- streamlit_app/pages/3_Classifier.py
+++ streamlit_app/pages/3_Classifier.py
@@ -1,5 +1,7 @@
 import streamlit as st #type: ignore
 from utils.utils import Model_utils, Image 
+import tensorflow as tf #type: ignore
+import cv2 #type: ignore
 
 def main():
 
@@ -10,6 +12,8 @@ def main():
     model = Model_utils.create_model()
     model.load_weights("models/crack_classifier/cp.ckpt")
 
+    
+
     uploaded_file = st.sidebar.file_uploader("Importer une image", type=["png", "jpg", "jpeg","tif"])
     
     if uploaded_file is not None:
@@ -17,8 +21,13 @@ def main():
         image = Image.load_streamlit(uploaded_file)
         
         if st.button("Détecter"):
+            img_path = "IMG/VN_vis_New.png"
+            image = cv2.imread(img_path,cv2.IMREAD_UNCHANGED)
             image = Model_utils.predict_and_draw_boxes(image, model)
+            #tf.keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
             st.image(image, use_column_width=True, caption="Image prédite avec les fissures détectées")
+            Image.save("IMG/output.png", image)
+
     
 
 if __name__ == "__main__":
